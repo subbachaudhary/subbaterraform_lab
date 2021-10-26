57fe36da29bfa1c529d4724e95636f1e4dd8a2f3
chauadhary, subba <subbachaudhary123@gmail.com>
2021-10-26 11:13:20 +0545

    last commit

--- test.tf
+++ test.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    access_key = "AKIARB7GQVCTFOWS3HK7"
-    secret_key = "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"
+    access_key = "AKIARB7GQVCTKYGDSFEA"
+    secret_key = "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"
     region = "us-east-2"
 }
 resource "aws_instance" "Myfirst_instance" {
-    ami = "ami-0629230e074c580f2"
+    ami = "ami-03b47d2d727e13114"
     instance_type = "t2.micro"
 }
