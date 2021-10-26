23302ca7bbcebf65b87d0607a694737aa01cd6c1
chauadhary, subba <subbachaudhary123@gmail.com>
2021-10-26 10:10:02 +0545

    this commit by subba saheb chaudhary

--- test.tf
+++ test.tf
@@ -1,8 +1,9 @@
-provider "aws"{
-    profile = "default"
+provider "aws" {
+    ACCESS_KEY = "AKIARB7GQVCTFOWS3HK7"
+    secret_key = "uLCNQZUev6Qu9Q6grOz91QVpK6ceQfcCQ6nD4ZTt"
     region = "us-east-1"
 }
-resource "aws_instance" "app_server"{
-    ami = "default"
+resource "aws_instance" "example" {
+    ami = "ami-03ad51d0b69da65e7"
     instance_type = "t2.micro"
 }
